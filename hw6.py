4c1e67f8b7ffa77aec3f8dca0c124d78d25f068e
chlee9610 <163001563+chlee9610@users.noreply.github.com>
2024-04-25 15:22:06 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,12 @@
+for j in range(1,10,1) :
+    print(f'{2} x {j} = {2*j:2d}', end='     ')
+    print(f'{3} x {j} = {3*j:2d}', end='     ')
+    print(f'{4} x {j} = {4*j:2d}', end='     ')
+    print(f'{5} x {j} = {5*j:2d}')
+
+print()
+for i in range(1,10,1) :
+    print(f'{6} x {i} = {6*i:2d}', end='     ')
+    print(f'{7} x {i} = {7*i:2d}', end='     ')
+    print(f'{8} x {i} = {8*i:2d}', end='     ')
+    print(f'{9} x {i} = {9*i:2d}')
